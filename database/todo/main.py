84d244b87407d0421e416ae2f7e400f8fe22c20a
Kai <shadowkai0121@gmail.com>
2022-06-04 13:36:53 +0800

    [database][todo][綁定按鈕功能]

--- database/todo/main.py
+++ database/todo/main.py
@@ -11,6 +11,25 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # signal & slot
+        self.ui.create_button.released.connect(self.create_todo)
+        self.ui.done_button.released.connect(self.done_todo)
+        self.ui.delete_button.released.connect(self.delete_todo)
+
+    def delete_todo(self):
+        # 測試按鈕功能
+        self.ui.create_todo_text_edit.setText('delete todo')
+
+    def done_todo(self):
+        # 測試按鈕功能
+        self.ui.create_todo_text_edit.setText('done todo')
+
+    def create_todo(self):
+        # 取得輸入框資料
+        todo = self.ui.create_todo_text_edit.text()
+        self.ui.todo_list.addItem(todo)
+
+
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
