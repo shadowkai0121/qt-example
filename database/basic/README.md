f0472df7a5428d22013d8697bf2497443decf8d0
Kai <shadowkai0121@gmail.com>
2022-06-04 14:07:11 +0800

    [database][basic][執行語法]

--- database/basic/README.md
+++ database/basic/README.md
@@ -1,2 +1,4 @@
 # Qt SQL 基本操作
 1. [開啟連線](./create_connection.py)
+2. [執行靜態語法](./static_sql_query.py)
+
---
+++ database/basic/static_sql_query.py
@@ -0,0 +1,30 @@
+import sys
+from PySide6.QtSql import QSqlQuery
+
+# 建立連線
+from create_connection import connection
+
+# 檢查連線
+if not connection.isOpen():
+    print(f'Database Error: {connection.lastError().databaseText()}')
+    sys.exit(1)
+
+# SQL 語法
+create_table_sql = '''
+CREATE TABLE todo_list (
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+    todo VARCHAR(50) NOT NULL,
+    is_done TINYINT(1)
+)
+'''
+
+# 使用 QSqlQuery 物件
+query = QSqlQuery()
+result = query.exec(create_table_sql)
+
+# table 重複的狀況會回傳 False，但是不算 Error
+print(f'create table result: {result}')
+if not result:
+    print(f'Database Error: {connection.lastError().databaseText()}')
+    sys.exit(1)
+
